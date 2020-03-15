963f86d4b37e9afc0fdbb6e0c6f8b8b6c5eb977e
Runnic <pedro.vieira3@fatecitapetininga.edu.br>
2020-03-15 18:31:20 -0300

    v0.2

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <TextInput />
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={styles.input}
+          />
         <Button title="Add"/>
       </View>
       <View>
@@ -16,5 +19,24 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50,
+    // height: '100%',
+    // width: '100%', 
+    //justifyContent: 'center',
+    // backgroundColor: 'azure',
+  },
+
+  input: { 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10,
+    width: '80%',
+  },
+
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  }
 });
